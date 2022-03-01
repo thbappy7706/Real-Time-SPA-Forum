9b98256fc9c5d8ddc57204839c205f1a3c0a4c2d
Bappy <tanvir.bappy@bansberrysg.com>
2022-03-01 16:03:59 +0600

    eloquent relationship is done

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Reply extends Model
 {
     use HasFactory;
+
+    public function question() : BelongsTo
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function user() :BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes() :HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
 }
